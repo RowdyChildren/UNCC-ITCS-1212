18ceddbe49496a3e3e38a7c655b4acbb0867e608
Riley Childs <riley@tfsgeo.com>
2015-08-28 16:15:46 +0000

    modified folder structure and added comments to some files

--- labs/lab1/calculateDiscount.cpp
+++ labs/lab1/calculateDiscount.cpp
@@ -2,7 +2,7 @@
 	Program: calculateDiscount.cpp
 	Date: 08/28/2015
 	Programmers: Riley Childs <me@rileychilds.me>
-Purpose:	This program prompts the user to enter the original price of the item and the percent discount. It calculates the new price and displays it on the screen. 
+	Purpose:	This program prompts the user to enter the original price of the item and the percent discount. It calculates the new price and displays it on the screen. 
 */
 #include<iostream>
 using namespace std;
@@ -14,13 +14,14 @@ int main()
     	double newPrice;
 	    // Accept values from the user
     	cout << "Enter the original price of the item you are buying:" << endl;
-    	cin >> originalPrice;
+    		cin >> originalPrice;
     	cout << "Enter discount percent:" << endl;
-    	cin >> percentDiscount;
+    		cin >> percentDiscount;
 	    // Calculate the new price
+	    //error was we needed to devide the discount entered by the use by 100 to generate a decimal
 	    percentDiscount = percentDiscount / 100;
     	newPrice = originalPrice * percentDiscount;
 		// Display the new price on the screen
     	cout << "The original price of $" << originalPrice << " has been reduced to: $" << newPrice;	
-	return 0; 
+		return 0; 
     }
