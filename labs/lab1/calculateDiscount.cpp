ef2c6e76286a68d6ee2a19d677f0eb5187914e4a
Riley Childs <riley@tfsgeo.com>
2015-08-29 16:56:21 +0000

    add error checking to the discount calculator

--- labs/lab1/calculateDiscount.cpp
+++ labs/lab1/calculateDiscount.cpp
@@ -5,6 +5,7 @@
 	Purpose:	This program prompts the user to enter the original price of the item and the percent discount. It calculates the new price and displays it on the screen. 
 */
 #include<iostream>
+#include<limits>
 using namespace std;
 int main()	
     {
@@ -12,11 +13,51 @@ int main()
     	double originalPrice;
 	    double percentDiscount;
     	double newPrice;
+    	bool rightInteger;
 	    // Accept values from the user
     	cout << "Enter the original price of the item you are buying:" << endl;
-    		cin >> originalPrice;
+    		while (!(cin >> originalPrice)) {
+    		
+    		//we need to clear the cin value
+    			cin.clear();
+    		//lets make sure that we don't take the wrong input!	
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    			cout << "That wasn't good input!\nEnter the original price of the item you are buying: (ex. 5.45):";
+    						}
     	cout << "Enter discount percent:" << endl;
-    		cin >> percentDiscount;
+    	
+    	//make sure their input is valid
+    	while (!(cin >> percentDiscount)) {
+    		
+    		//we need to clear the cin value
+    			cin.clear();
+    		//lets make sure that we don't take the wrong input!	
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    			cout << "That wasn't good input!\nEnter the Percent discount (ex. XX.X% where you enter XX.X and not the %):";
+    						}
+    	//their input should be above or equal to 1
+    	while (!rightInteger)
+	{
+		if (percentDiscount >= 1)
+		{
+		rightInteger = true;
+		}
+			else
+			{
+			cout << "That wasn't good input!\nEnter the Percent discount (ex. XX.X% where you enter XX.X and not the %):" << endl;
+				//well it wasn't greater than 1 so they have to try again.
+			while (!(cin >> percentDiscount)) {
+    		
+    		//we need to clear the cin value
+    			cin.clear();
+    		//lets make sure that we don't take the wrong input!	
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    			cout << "That wasn't good input!\nEnter the Percent discount (ex. XX.X% where you enter XX.X and not the %):";
+    						}
+			}
+	}
+	
+    		
 	    // Calculate the new price
 	    //error was we needed to devide the discount entered by the use by 100 to generate a decimal
 	    percentDiscount = percentDiscount / 100;
