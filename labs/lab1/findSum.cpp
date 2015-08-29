268c28451cf2f455c87ff19c11c873d0ca0312bb
Riley Childs <riley@tfsgeo.com>
2015-08-29 17:04:34 +0000

    change number var to a float in findSum.cpp, also added error checking there

--- labs/lab1/findSum.cpp
+++ labs/lab1/findSum.cpp
@@ -7,19 +7,26 @@
 
 
 #include <iostream> 
-
+#include <limits>
 using namespace std; 
 
 int main() 
 	{ 
-		int number; 
+		float number; 
 		
 		float total; 
 		
 		cout << "Today is a great day for Lab"; 
 		
 		cout << endl << "Let's start off by typing a number of your choice" << endl; 
-			cin >> number; 
+			while (!(cin >> number)) {
+    		// input was not an integer
+    		//we need to clear the cin value
+    			cin.clear();
+    		//lets make sure that we don't take the wrong input!	
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    			cout << "Thats not a number!!\nTry again:";
+    						}
 			
 		total = number * 2; 
 		cout << total << " is twice the number you typed" << endl; 
