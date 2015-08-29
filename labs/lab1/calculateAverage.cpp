c51673bd9953187079bc7873690db5504a96f169
Riley Childs <riley@tfsgeo.com>
2015-08-29 16:37:16 +0000

    make calculateAverage.cpp do some error checking

--- labs/lab1/calculateAverage.cpp
+++ labs/lab1/calculateAverage.cpp
@@ -1,11 +1,12 @@
 /*
 	Program: calculateAverage.cpp
 	Date: 08/28/2015
-	Programmers: Riley Childs <me@rileychilds.me
+	Programmers: Riley Childs <me@rileychilds.me>
     Purpose:	This program prompts the user to enter 3 test scores. It reads in these scores, calculates the average and prints the average to the screen.
 
 */
 #include<iostream>
+#include<limits>
 using namespace std;
 int main()	
     {
@@ -13,16 +14,38 @@ int main()
 	    float test1, test2, test3, average;
 	// Accept test values from the user
 	    cout << "Enter the first test grade:";
-	        cin >> test1;
+	     
+	        while (!(cin >> test1)) {
+    		// input was not an integer
+    		//we need to clear the cin value
+    			cin.clear();
+    		//lets make sure that we don't take the wrong input!	
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    			cout << "That wasn't good input!\nEnter the first test grade:";
+    						}			
 	    cout << "Enter the second test grade:";
-	        cin >> test2;
+	 		while (!(cin >> test2)) {
+    		// input was not an integer
+    		//we need to clear the cin value
+    			cin.clear();
+    		//lets make sure that we don't take the wrong input!	
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    			cout << "That wasn't good input!\nEnter the second test grade:";
+    						}
 	    cout << "Enter the third test grade:";
-	        cin >> test3;
+	       	while (!(cin >> test3)) {
+    		// input was not an integer
+    		//we need to clear the cin value
+    			cin.clear();
+    		//lets make sure that we don't take the wrong input!	
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    			cout << "That wasn't good input!\nEnter the while test grade:";
+	       	}
 
 	    // Calculate the Average of the 3 test 
 	    //logic error: you needed to add preentacess 
 	    average = (test1 + test2 + test3) / 3;
 	    // Display the average on the screen
-	    cout << "The average of" << test1 << " and" << test2 << " and " << test3 <<" is:" << average;
+	    cout << "The average of" << test1 << " and " << test2 << " and " << test3 <<" is:" << average;
 	    return 0; 
     }
