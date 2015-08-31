07e3c0274b1c9ca23ccd121c1098d4b3cdb70530
Riley Childs <riley@tfsgeo.com>
2015-08-31 16:09:34 +0000

    add ch2Example1.cpp

---
+++ classwork/aug31ch2/ch2Example1.cpp
@@ -0,0 +1,18 @@
+/*	Program: ch2Example1.cpp
+	Date: 08/31/2015
+	Programmers: Riley Childs <me@rileychilds.me>
+	Purpose: Demo in class
+
+*/
+
+#include <iostream>
+
+namespace std;
+
+int main() {
+    
+    
+    
+    
+    
+} //int main
